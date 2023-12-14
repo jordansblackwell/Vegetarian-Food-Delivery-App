50226b7199611a4553e86b674947d03561fbf50b
Jordan <jordansblackwell@gmail.com>
2023-12-14 13:56:46 -0500

    finished app

---
+++ src/data/data.js
@@ -0,0 +1,169 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Tropical Acai',
+      category: 'Breakfast',
+      image:
+      'https://images.pexels.com/photos/14167805/pexels-photo-14167805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 2,
+      name: 'Rice and Stew',
+      category: 'Lunch and Dinner',
+      image:
+        'https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 3,
+      name: 'Beet Salad',
+      category: 'Light Bites',
+      image:
+        'https://images.pexels.com/photos/8696556/pexels-photo-8696556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$',
+    },
+    {
+      id: 4,
+      name: 'Raspberry Cheesecake',
+      category: 'Desserts',
+      image:
+        'https://images.pexels.com/photos/3185509/pexels-photo-3185509.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 5,
+      name: 'Spicy Pasta',
+      category: 'Lunch and Dinner',
+      image:
+        'https://images.pexels.com/photos/10456299/pexels-photo-10456299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 6,
+      name: 'Black Bean Burger',
+      category: 'Lunch and Dinner',
+      image:
+        'https://images.pexels.com/photos/3607284/pexels-photo-3607284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 7,
+      name: 'Buckwheat Blueberry Pancakes',
+      category: 'Breakfast',
+      image:
+        'https://images.pexels.com/photos/3780469/pexels-photo-3780469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 8,
+      name: 'Lemony Roasted Potatoes',
+      category: 'Light Bites',
+      image:
+        'https://images.pexels.com/photos/8250190/pexels-photo-8250190.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 9,
+      name: 'Matcha Brownies',
+      category: 'Desserts',
+      image:
+        'https://serenetrail.com/wp-content/uploads/2023/05/Vegan-matcha-brownies-with-white-chocolate.jpg',
+      price: '$',
+    },
+    {
+      id: 10,
+      name: 'Avocado Toast',
+      category: 'Breakfast',
+      image:
+        'https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 11,
+      name: 'Tofu Caesar Wrap',
+      category: 'Lunch and Dinner',
+      image:
+        'https://tastythriftytimely.com/wp-content/uploads/2023/07/Caesar-Wrap-7-1367x2048.jpg',
+      price: '$$$',
+    },
+    {
+      id: 12,
+      name: 'Fruit Salad',
+      category: 'Light Bites',
+      image:
+        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$',
+    },
+    {
+      id: 13,
+      name: 'Tempura Cauliflower',
+      category: 'Lunch and Dinner',
+      image:
+        'https://images.pexels.com/photos/5638544/pexels-photo-5638544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 14,
+      name: 'Hummus and Pita',
+      category: 'Light Bites',
+      image:
+        'https://images.pexels.com/photos/1618898/pexels-photo-1618898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 15,
+      name: 'Passionfruit Overnight Oats',
+      category: 'Breakfast',
+      image:
+        'https://images.pexels.com/photos/128865/pexels-photo-128865.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 16,
+      name: 'Vegetable Fried Rice with Tofu',
+      category: 'Lunch and Dinner',
+      image:
+        'https://images.pexels.com/photos/7758253/pexels-photo-7758253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+  ];
+  
+  export const categories = [
+    {
+      id: 1,
+      name: 'Speedy Service',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png',
+    },
+    {
+      id: 2,
+      name: 'Current Promotions',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
+    },
+    {
+      id: 5,
+      name: 'Latest Deals',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+    },
+    {
+      id: 6,
+      name: 'Restaurant Rewards',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+    },
+    {
+      id: 7,
+      name: 'Best Overall',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+    },
+    {
+      id: 8,
+      name: 'Shipped Free',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+    },
+  ];
